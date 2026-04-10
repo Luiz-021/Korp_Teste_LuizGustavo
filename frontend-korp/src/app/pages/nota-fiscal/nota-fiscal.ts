04abb289a1abf3ce90374c29224cd27a921ed9a3
Luiz-021 <luizgute@outlook.com>
2026-04-10 19:30:54 -0300

    chore(frontend): setup inicial do angular, rotas, material design e http client

---
+++ frontend-korp/src/app/pages/nota-fiscal/nota-fiscal.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nota-fiscal',
+  imports: [],
+  templateUrl: './nota-fiscal.html',
+  styleUrl: './nota-fiscal.css',
+})
+export class NotaFiscal {}
